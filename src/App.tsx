4ebae6a2b58e202443fe9507ba6636cb1caf3da2
artenlf <felipe.arten@gmail.com>
2022-09-17 20:01:14 -0300

    Codes - lesson 3 - List component

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import React from "react";
 import Form from "./components/Form/Form";
+import List from "./components/List/List";
 
 function App() {
   return (
     <div className="App">
       <Form />
+      <List />
     </div>
   );
 }
